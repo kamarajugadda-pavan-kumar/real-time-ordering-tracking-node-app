e50d0d2b008a09b9bb05921bc6f71c3631212f3b
pavan kumar kamarajugadda <pavan@codeconnex.com>
2022-09-16 20:00:57 +0530

    16-sep-2022

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const ejs=require("ejs")
+const expressLayout =require('express-ejs-layouts')
+const path=require('path')
+const PORT = process.env.PORT || 3000;
+
+
+
+
+// assets
+app.use(express.static('public'))
+
+// set template engine
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine',"ejs")
+
+//home 
+app.get("/",function (req,res){
+    res.render('home')
+})
+
+// cart
+app.get("/cart",(req,res)=>{
+    res.render('customers/cart')
+})
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
